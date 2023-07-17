3afb49e4cc9eed12dbe60be653df838ccacac13b
Luca Terrazzan <luca.terraz@gmail.com>
2023-07-17 12:31:03 +0200

    chore(lint)

--- src/middleware/github-username.middleware.ts
+++ src/middleware/github-username.middleware.ts
@@ -1,5 +1,5 @@
-import { Request, Response, NextFunction } from 'express'
-import { Octokit } from 'octokit'
+import { Request, Response, NextFunction } from 'express';
+import { Octokit } from 'octokit';
 
 declare global {
   namespace Express {
@@ -11,28 +11,28 @@ declare global {
   }
 }
 
-const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN })
+const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
 
-const orgName = process.env.GITHUB_ORG || ''
+const orgName = process.env.GITHUB_ORG || '';
 
 export const GithubUser = async (
   req: Request,
   res: Response,
   next: NextFunction,
 ) => {
-  const { username } = req.params
+  const { username } = req.params;
   req.context = {
     username: username,
-  }
+  };
   try {
     await octokit.rest.orgs.getMembershipForUser({
       org: orgName,
       username: username,
-    })
-    next()
+    });
+    next();
   } catch (e) {
     return res.status(401).json({
       error: `you must be part of the ${orgName} organisation to use this api`,
-    })
+    });
   }
-}
+};
--- src/middleware/validate.ts
+++ src/middleware/validate.ts
@@ -1,47 +1,43 @@
-import { NextFunction, Request, Response } from 'express'
-import { AnyZodObject, ZodError, ZodFormattedError } from 'zod'
-
-interface FormattedError {
-  issues: string[]
-}
+import { NextFunction, Request, Response } from 'express';
+import { AnyZodObject, ZodError } from 'zod';
 
 const formatErrorPart = <T>(
   part: string,
   errors: Record<string, [] | Record<string, []>>,
 ): string[] => {
-  const issues: string[] = []
+  const issues: string[] = [];
   if (!errors) {
-    return issues
+    return issues;
   }
-  const values = Object.values(errors)
+  const values = Object.values(errors);
   for (const value of values) {
     if (!Array.isArray(value)) {
-      const nestedIssues = formatErrorPart(part, value)
-      issues.push(...nestedIssues)
-      continue
+      const nestedIssues = formatErrorPart(part, value);
+      issues.push(...nestedIssues);
+      continue;
     }
-    const mappedIssues = value.map((issue) => `${part}: ${issue}`)
-    issues.push(...mappedIssues)
+    const mappedIssues = value.map((issue) => `${part}: ${issue}`);
+    issues.push(...mappedIssues);
   }
-  return issues
-}
+  return issues;
+};
 
 const formatError = (error: ZodError): string[] => {
   const paramsIssues = formatErrorPart(
     'params',
     (error.format() as unknown as any).params,
-  )
+  );
   const bodyIssues = formatErrorPart(
     'body',
     (error.format() as unknown as any).body,
-  )
+  );
   const queryIssues = formatErrorPart(
     'query',
     (error.format() as unknown as any).query,
-  )
-  const issues = [...paramsIssues, ...bodyIssues, ...queryIssues]
-  return issues
-}
+  );
+  const issues = [...paramsIssues, ...bodyIssues, ...queryIssues];
+  return issues;
+};
 
 export const Validate = (schema: AnyZodObject) => {
   return (req: Request, res: Response, next: NextFunction) => {
@@ -50,14 +46,14 @@ export const Validate = (schema: AnyZodObject) => {
         body: req.body,
         query: req.query,
         params: req.params,
-      })
-      return next()
+      });
+      return next();
     } catch (error) {
       if (error instanceof ZodError) {
-        const issues = formatError(error)
-        return res.status(400).json({ error: issues })
+        const issues = formatError(error);
+        return res.status(400).json({ error: issues });
       }
-      return res.status(400).json(error)
+      return res.status(400).json(error);
     }
-  }
-}
+  };
+};
