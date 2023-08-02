3ae00172b6e93feaae9e5413d840126500f267c0
Lewis <ljoncb@gmail.com>
2023-08-02 08:30:22 +0100

    greengrocers extension

--- src/fruit/grocery.controller.ts
+++ src/fruit/grocery.controller.ts
@@ -1,10 +1,10 @@
-import { Request, Response } from 'express';
+import { Request, Response } from 'express'
 
 interface Grocery {
-  id: string,
-  name: string,
-  type: 'fruit' | 'vegetable',
-  price: number,
+  id: string
+  name: string
+  type: 'fruit' | 'vegetable'
+  price: number
 }
 
 const GROCERIES_LIST: Grocery[] = [
@@ -12,66 +12,74 @@ const GROCERIES_LIST: Grocery[] = [
     id: '001-beetroot',
     name: 'beetroot',
     type: 'vegetable',
-    price: 0.35
+    price: 0.35,
   },
   {
     id: '002-carrot',
     name: 'carrot',
     type: 'vegetable',
-    price: 0.35
+    price: 0.35,
   },
   {
     id: '003-apple',
     name: 'apple',
     type: 'fruit',
-    price: 0.35
+    price: 0.35,
   },
   {
     id: '004-apricot',
     name: 'apricot',
     type: 'fruit',
-    price: 0.35
+    price: 0.35,
   },
   {
     id: '005-avocado',
     name: 'avocado',
     type: 'fruit',
-    price: 0.35
+    price: 0.35,
   },
   {
     id: '006-bananas',
     name: 'bananas',
     type: 'fruit',
-    price: 0.35
+    price: 0.35,
   },
   {
     id: '007-bell-pepper',
     name: 'bell pepper',
     type: 'vegetable',
-    price: 0.35
+    price: 0.35,
   },
   {
     id: '008-berry',
     name: 'cherry',
     type: 'fruit',
-    price: 0.35
+    price: 0.35,
   },
   {
     id: '009-blueberry',
     name: 'blueberry',
     type: 'fruit',
-    price: 0.35
+    price: 0.35,
   },
   {
     id: '010-eggplant',
     name: 'eggplant',
     type: 'vegetable',
-    price: 0.35
-  }
-];
+    price: 0.35,
+  },
+]
 
 export class GroceryController {
-  public getGroceriesList(_req: Request, res: Response) {
-    return res.status(200).json(GROCERIES_LIST);
+  public getGroceriesList(req: Request, res: Response) {
+    const { type } = req.query
+    if (!type) {
+      return res.status(200).json(GROCERIES_LIST)
+    }
+    if (type !== 'fruit' && type !== 'vegetable') {
+      return res.status(400).json({ error: `invalid type value: "${type}"` })
+    }
+    const items = GROCERIES_LIST.filter((item) => item.type === type)
+    return res.status(200).json(items)
   }
 }
