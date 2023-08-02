8ccb2def65b01b99fc842e8cafbdbb36c0b1c268
Lewis <ljoncb@gmail.com>
2023-08-02 12:04:14 +0100

    changed prices

--- src/fruit/grocery.controller.ts
+++ src/fruit/grocery.controller.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from 'express';
+import { Request, Response } from 'express'
 
 interface Grocery {
   id: string
@@ -18,68 +18,68 @@ const GROCERIES_LIST: Grocery[] = [
     id: '002-carrot',
     name: 'carrot',
     type: 'vegetable',
-    price: 0.35,
+    price: 0.2,
   },
   {
     id: '003-apple',
     name: 'apple',
     type: 'fruit',
-    price: 0.35,
+    price: 0.23,
   },
   {
     id: '004-apricot',
     name: 'apricot',
     type: 'fruit',
-    price: 0.35,
+    price: 0.4,
   },
   {
     id: '005-avocado',
     name: 'avocado',
     type: 'fruit',
-    price: 0.35,
+    price: 1.15,
   },
   {
     id: '006-bananas',
     name: 'bananas',
     type: 'fruit',
-    price: 0.35,
+    price: 0.3,
   },
   {
     id: '007-bell-pepper',
     name: 'bell pepper',
     type: 'vegetable',
-    price: 0.35,
+    price: 0.5,
   },
   {
     id: '008-berry',
     name: 'cherry',
     type: 'fruit',
-    price: 0.35,
+    price: 0.33,
   },
   {
     id: '009-blueberry',
     name: 'blueberry',
     type: 'fruit',
-    price: 0.35,
+    price: 0.33,
   },
   {
     id: '010-eggplant',
     name: 'eggplant',
     type: 'vegetable',
-    price: 0.35,
+    price: 0.7,
   },
-];
+]
 
 export class GroceryController {
   public getGroceriesList(req: Request, res: Response) {
-    const { type } = req.query;
+    const { type } = req.query
     if (!type) {
-      return res.status(200).json(GROCERIES_LIST);
+      return res.status(200).json(GROCERIES_LIST)
     }
     if (type !== 'fruit' && type !== 'vegetable') {
-      return res.status(400).json({ error: `invalid type value: "${type}"` });
+      return res.status(400).json({ error: `invalid type value: "${type}"` })
     }
-    const items = GROCERIES_LIST.filter((item) => item.type === type);
-    return res.status(200).json(items);
+    const items = GROCERIES_LIST.filter((item) => item.type === type)
+    return res.status(200).json(items)
   }
 }
