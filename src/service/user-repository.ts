3c72996885f064093bb962590cb1d9d8fc7ff33b
Nathan King <nathanking3590@gmail.com>
2023-11-06 12:25:45 +0000

    refactor repository layer

---
+++ src/service/user-repository.ts
@@ -0,0 +1,34 @@
+import { Identity } from "./identity";
+
+export abstract class UserRepository<T extends Identity, CreateDto, UpdateDto> {
+    protected data: T[] = [];
+    protected id = 1;
+
+    abstract create(dto: CreateDto): T;
+
+    getAll(): T[] {
+        return this.data;
+    }
+
+    getById(id: number): T | undefined {
+        return this.data.find(item => item.id === id);
+    }
+
+    update(id: number, dto: UpdateDto): T | undefined {
+        const item = this.getById(id);
+        if (!item) {
+            return item;
+        }
+        Object.assign(item, dto);
+        return item;
+    }
+
+    delete(id: number): T | undefined {
+        const index = this.data.findIndex((item) => item.id === id);
+        if (index === -1) {
+            return undefined;
+        }
+        const item = this.data.splice(index, 1);
+        return item[0];
+    }
+}
--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -1,11 +1,11 @@
 import { Response } from 'express';
-import { TodoRepository } from './todo.repository';
 import { UserTodoRepository } from './user-todo.repository';
 import { CreateTodoDto } from './dto/create-todo.dto';
 import { UpdateTodoDto } from './dto/update-todo.dto';
 import { Context } from '../middleware/context';
+import { Repository } from '../service/repository';
 
-const repo = new TodoRepository();
+const repo = new Repository<UserTodoRepository>(UserTodoRepository);
 
 const getRepoFromUsername = (req: Context): UserTodoRepository => {
   const username = req.context?.username || '';
@@ -16,7 +16,7 @@ const getRepoFromUsername = (req: Context): UserTodoRepository => {
 export const GetAllTodos = (req: Context, res: Response) => {
   const todoRepo = getRepoFromUsername(req);
 
-  const todos = todoRepo.getTodos();
+  const todos = todoRepo.getAll();
 
   return res.status(200).json(todos);
 };
@@ -25,7 +25,7 @@ export const GetTodoByID = (req: Context, res: Response) => {
   const id = Number(req.params.id);
   const todoRepo = getRepoFromUsername(req);
 
-  const todo = todoRepo.getTodoById(id);
+  const todo = todoRepo.getById(id);
   if (!todo) {
     return res.status(404).json({ error: 'todo not found' });
   }
@@ -36,7 +36,7 @@ export const CreateTodo = (req: Context, res: Response) => {
   const dto: CreateTodoDto = req.body;
   const todoRepo = getRepoFromUsername(req);
 
-  const todo = todoRepo.createTodo(dto);
+  const todo = todoRepo.create(dto);
 
   return res.status(201).json(todo);
 };
@@ -46,7 +46,7 @@ export const UpdateTodo = (req: Context, res: Response) => {
   const dto: UpdateTodoDto = req.body;
   const todoRepo = getRepoFromUsername(req);
 
-  const todo = todoRepo.updateTodo(id, dto);
+  const todo = todoRepo.update(id, dto);
   if (!todo) {
     return res.status(404).json({ error: 'todo not found' });
   }
@@ -58,7 +58,7 @@ export const DeleteTodo = (req: Context, res: Response) => {
   const id = Number(req.params.id);
   const todoRepo = getRepoFromUsername(req);
 
-  const todo = todoRepo.deleteTodo(id);
+  const todo = todoRepo.delete(id);
   if (!todo) {
     return res.status(404).json({ error: 'todo not found' });
   }
