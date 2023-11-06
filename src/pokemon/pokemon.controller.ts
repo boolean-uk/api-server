857e245112510ac2ea27749bbe929a3f8de5f38b
Nathan King <nathanking3590@gmail.com>
2023-11-06 16:43:10 +0000

    remove redundant code

--- src/pokemon/pokemon.controller.ts
+++ src/pokemon/pokemon.controller.ts
@@ -1,54 +1,7 @@
-import { Response } from 'express';
-import { Context } from '../middleware/context';
 import { UserPokemonRepository } from './user-pokemon.repository';
-import { CreatePokemonDto } from './dto/create-pokemon.dto';
 import { Repository } from '../service/repository';
+import { Controller } from '../service/controller';
 
 const repo = new Repository<UserPokemonRepository>(UserPokemonRepository);
 
-const getRepoFromUsername = (req: Context): UserPokemonRepository => {
-  const username = req.context?.username || '';
-  return repo.getOrCreateUserRepository(username);
-};
-
-export const GetAllPokemon = (req: Context, res: Response) => {
-  const pokemonRepo = getRepoFromUsername(req);
-  
-  const pokemon = pokemonRepo.getAll();
-  
-  return res.status(200).json(pokemon);
-};
-
-export const CreatePokemon = (req: Context, res: Response) => {
-  const dto: CreatePokemonDto = req.body;
-  const pokemonRepo = getRepoFromUsername(req);
-
-  const pokemon = pokemonRepo.create(dto);
-
-  return res.status(201).json(pokemon);
-};
-
-export const DeletePokemon = (req: Context, res: Response) => {
-  const id = Number(req.params.id);
-  const pokemonRepo = getRepoFromUsername(req);
-  
-  const pokemon = pokemonRepo.delete(id);
-  if (!pokemon) {
-    return res.status(404).json({ error: 'pokemon not found' });
-  }
-  
-  return res.status(200).json(pokemon);
-};
-
-export const UpdatePokemon = (req: Context, res: Response) => {
-  const id = Number(req.params.id);
-  const dto: CreatePokemonDto = req.body;
-  const pokemonRepo = getRepoFromUsername(req);
-  
-  const pokemon = pokemonRepo.update(id, dto);
-  if (!pokemon) {
-    return res.status(404).json({ error: 'pokemon not found' });
-  }
-  
-  return res.status(200).json(pokemon);
-};
+export const controller = new Controller(repo);
--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -1,66 +1,7 @@
-import { Response } from 'express';
 import { UserTodoRepository } from './user-todo.repository';
-import { CreateTodoDto } from './dto/create-todo.dto';
-import { UpdateTodoDto } from './dto/update-todo.dto';
-import { Context } from '../middleware/context';
 import { Repository } from '../service/repository';
+import { Controller } from '../service/controller';
 
 const repo = new Repository<UserTodoRepository>(UserTodoRepository);
 
-const getRepoFromUsername = (req: Context): UserTodoRepository => {
-  const username = req.context?.username || '';
-  return repo.getOrCreateUserRepository(username);
-};
-
-export const GetAllTodos = (req: Context, res: Response) => {
-  const todoRepo = getRepoFromUsername(req);
-
-  const todos = todoRepo.getAll();
-
-  return res.status(200).json(todos);
-};
-
-export const GetTodoByID = (req: Context, res: Response) => {
-  const id = Number(req.params.id);
-  const todoRepo = getRepoFromUsername(req);
-
-  const todo = todoRepo.getById(id);
-  if (!todo) {
-    return res.status(404).json({ error: 'todo not found' });
-  }
-  return res.status(200).json(todo);
-};
-
-export const CreateTodo = (req: Context, res: Response) => {
-  const dto: CreateTodoDto = req.body;
-  const todoRepo = getRepoFromUsername(req);
-
-  const todo = todoRepo.create(dto);
-
-  return res.status(201).json(todo);
-};
-
-export const UpdateTodo = (req: Context, res: Response) => {
-  const id = Number(req.params.id);
-  const dto: UpdateTodoDto = req.body;
-  const todoRepo = getRepoFromUsername(req);
-
-  const todo = todoRepo.update(id, dto);
-  if (!todo) {
-    return res.status(404).json({ error: 'todo not found' });
-  }
-
-  return res.status(200).json(todo);
-};
-
-export const DeleteTodo = (req: Context, res: Response) => {
-  const id = Number(req.params.id);
-  const todoRepo = getRepoFromUsername(req);
-
-  const todo = todoRepo.delete(id);
-  if (!todo) {
-    return res.status(404).json({ error: 'todo not found' });
-  }
-
-  return res.status(200).json(todo);
-};
+export const controller = new Controller(repo);
